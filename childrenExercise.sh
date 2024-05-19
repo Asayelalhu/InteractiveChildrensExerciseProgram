1d5c4af06e92b0e4e10781dba4f799c02fcce112
Dairidha <daiialhasmii26@gmail.com>
2024-05-19 11:05:07 +0300

    dhai

--- childrenExercise.sh
+++ childrenExercise.sh
@@ -57,4 +57,102 @@ case "$category" in
         tech_exercise=$(tput setaf 6)"Choose the correct answer letter : Where do you watch cartoons?"$(tput sgr0)
         tech_options=("A) Book" "B) TV" "C) Bag")
         correct_tech_answer="B"
+        ;;"6-9")
+        math_exercise=$(tput setaf 6)"Choose the correct answer letter : Sum of 132 grapes + 8 grapes ="$(tput sgr0)
+        math_options=("A) 150" "B) 140" "C) 141")
+        correct_answer="B"
+        language_exercise=$(tput setaf 6)"Choose the correct answer letter : Complete the sentence: The boy go ...."$(tput sgr0)
+        language_options=("A) to school" "B) on school" "C) at school")
+        correct_language_answer="A"
+        ethics_exercise="$(tput setaf 6)Choose the correct answer letter : What would you do if you saw someone being bullied?"$(tput sgr0)
+        ethics_options=("A) Join in" "B) Ignore it" "C) Tell an adult")
+        correct_ethics_answer="C"
+        tech_exercise=$(tput setaf 6)"Choose the correct answer letter : Which of these applications is used for solving homework?"$(tput sgr0)
+        tech_options=("A) Roblox" "B) Madrasati" "C) Snapchat")
+        correct_tech_answer="B"
         ;;
+    "10-12")
+        math_exercise=$(tput setaf 6)"Choose the correct answer letter : 20 potatoes x 8 potatoes ="$(tput sgr0)
+        math_options=("A) 123" "B) 155" "C) 160")
+        correct_answer="C"
+        language_exercise=$(tput setaf 6)"Choose the correct answer letter : Identify the adjective in the sentence: 'The big, blue boat sailed quickly.'"$(tput sgr0)
+        language_options=("A) big" "B) blue" "C) quickly")
+        correct_language_answer="B"
+        ethics_exercise=$(tput setaf 6)"Choose the correct answer letter : What does 'integrity' mean?"$(tput sgr0)
+        ethics_options=("A) Being honest and having strong moral principles" "B) Being sneaky" "C) Being popular")
+        correct_ethics_answer="A"
+        tech_exercise=$(tput setaf 6)"Choose the correct answer letter : What is an example of a programming 
+        language?"$(tput sgr0)
+        tech_options=("A) English" "B) Java" "C) Spanish")
+        correct_tech_answer="B"
+        ;;
+esac
+
+echo "**"
+
+
+echo "Math exercises:"
+echo "$math_exercise"
+for option in "${math_options[@]}"; do
+    echo "$option"
+done
+
+read -p "Choose the correct answer for the math exercise: " user_answer
+
+user_answer=$(echo "$user_answer" | sed 's/./\U&/g')
+if [ "$user_answer" == "$correct_answer" ]; then
+    echo -e "\e[32mCorrect answer,Well done! Keep up the good work!\e[0m"
+else
+    echo -e "\e[31mWrong answer,Try again! You'll improve with practice.\e[0m"
+fi
+
+echo "**"
+6-9")
+        math_exercise=$(tput setaf 6)"Choose the correct answer letter : Sum of 132 grapes + 8 grapes ="$(tput sgr0)
+        math_options=("A) 150" "B) 140" "C) 141")
+        correct_answer="B"
+        language_exercise=$(tput setaf 6)"Choose the correct answer letter : Complete the sentence: The boy go ...."$(tput sgr0)
+        language_options=("A) to school" "B) on school" "C) at school")
+        correct_language_answer="A"
+        ethics_exercise="$(tput setaf 6)Choose the correct answer letter : What would you do if you saw someone being bullied?"$(tput sgr0)
+        ethics_options=("A) Join in" "B) Ignore it" "C) Tell an adult")
+        correct_ethics_answer="C"
+        tech_exercise=$(tput setaf 6)"Choose the correct answer letter : Which of these applications is used for solving homework?"$(tput sgr0)
+        tech_options=("A) Roblox" "B) Madrasati" "C) Snapchat")
+        correct_tech_answer="B"
+        ;;
+    "10-12")
+        math_exercise=$(tput setaf 6)"Choose the correct answer letter : 20 potatoes x 8 potatoes ="$(tput sgr0)
+        math_options=("A) 123" "B) 155" "C) 160")
+        correct_answer="C"
+        language_exercise=$(tput setaf 6)"Choose the correct answer letter : Identify the adjective in the sentence: 'The big, blue boat sailed quickly.'"$(tput sgr0)
+        language_options=("A) big" "B) blue" "C) quickly")
+        correct_language_answer="B"
+        ethics_exercise=$(tput setaf 6)"Choose the correct answer letter : What does 'integrity' mean?"$(tput sgr0)
+        ethics_options=("A) Being honest and having strong moral principles" "B) Being sneaky" "C) Being popular")
+        correct_ethics_answer="A"
+        tech_exercise=$(tput setaf 6)"Choose the correct answer letter : What is an example of a programming 
+        language?"$(tput sgr0)
+        tech_options=("A) English" "B) Java" "C) Spanish")
+        correct_tech_answer="B"
+        ;;
+esac
+
+echo "**"
+
+echo "Math exercises:"
+echo "$math_exercise"
+for option in "${math_options[@]}"; do
+    echo "$option"
+done
+
+read -p "Choose the correct answer for the math exercise: " user_answer
+
+user_answer=$(echo "$user_answer" | sed 's/./\U&/g')
+if [ "$user_answer" == "$correct_answer" ]; then
+   echo -e "\e[32mCorrect answer, Well done! Keep up the good work!\e[0m"
+else
+   echo -e "\e[31mWrong answer, Try again!
+You'll improve with practice. \e[0m"
+
+fi
